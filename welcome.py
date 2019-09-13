e129940df6ccd5bc754d20128cf603e17cd182d1
prakritii <55283565+prakritii@users.noreply.github.com>
2019-09-13 21:42:05 +0545

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,34 @@
+name = input('what is your Name?:')
+age = int(input('what is your age?:'))
+food = input('what is your favourite food?:')
+music = input('what is your favourite music?:')
+movie = input('what is your favourite movie?:')
+singer = input('who is your favourite singer?:')
+country = input('what is your country name?:')
+
+
+birthdate = 2076-age
+name_len = len(name)
+age_after10 = age + 10
+age_after100 = age + 100
+
+
+
+
+print('{a} jee its nice to meet you you were born on {b} years old'.format(a=name, b=birthdate))
+
+print('your name is {name_len} letters long'.format(name_len=name_len))
+print('in 10 years you will be {age10} years old'.format(age10=age_after10))
+print('in 100 years you will be {age100} years old'.format(age100=age_after100))
+print('your favourite food is {fav}'.format(fav=food))
+print('your favourite music is {fav}'.format(fav=music))
+print('your favourite singer is {fav}'.format(fav=singer))
+print('you are from {xyz}'.format(xyz=country))
+print('have a good day')
+
+
+#your name is yeti letters long
+#in 10 years you will be yeti years old
+#in 100 years you will be yeti years old
+#what is your favourite food
+#what is fav music,movie,singer,what is y9uor country 
